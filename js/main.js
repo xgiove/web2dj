9001fbccb93a6a0a93881bc145a28a059945ec2a
Paolo <paolorosettani97@gmail.com>
2025-06-15 14:23:33 +0200

    prima bozza

--- js/main.js
+++ js/main.js
@@ -28,39 +28,76 @@
     });
 
     /*------------------
-		Navigation
-	--------------------*/
+        Navigation
+    --------------------*/
     $(".mobile-menu").slicknav({
         prependTo: '#mobile-menu-wrap',
         allowParentLinks: true
     });
-    
+
     /*--------------------------
         Event Slider
     ----------------------------*/
-    $(".event__slider").owlCarousel({
+    // $(".event__slider").owlCarousel({
+    //     loop: true,
+    //     margin: 0,
+    //     items: 3,
+    //     dots: false,
+    //     nav: true,
+    //     navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+    //     smartSpeed: 1200,
+    //     autoHeight: false,
+    //     autoplay: true,
+    //     responsive: {
+    //         992: {
+    //             items: 3,
+    //         },
+    //         768: {
+    //             items: 2,
+    //         },
+    //         0: {
+    //             items: 1,
+    //         },
+    //     }
+    // });
+
+    const $carousel = $(".event__slider");
+
+    $carousel.owlCarousel({
         loop: true,
         margin: 0,
         items: 3,
         dots: false,
         nav: true,
-        navText: ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+        navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
         smartSpeed: 1200,
         autoHeight: false,
         autoplay: true,
+        autoplayTimeout: 5000, // 5 secondi
+        autoplayHoverPause: false,
         responsive: {
-            992: {
-                items: 3,
-            },
-            768: {
-                items: 2,
-            },
-            0: {
-                items: 1,
-            },
+            992: { items: 3 },
+            768: { items: 2 },
+            0: { items: 1 },
         }
     });
-    
+
+    // Intercetta le interazioni manuali e resetta il timer dell'autoplay
+    function resetAutoplay() {
+        $carousel.trigger('stop.owl.autoplay');
+        $carousel.trigger('play.owl.autoplay');
+    }
+
+    // Quando clicchi le frecce
+    $carousel.on('click', '.owl-prev, .owl-next', function () {
+        resetAutoplay();
+    });
+
+    // Quando fai swipe (drag)
+    $carousel.on('drag.owl.carousel', function () {
+        resetAutoplay();
+    });
+
     /*--------------------------
         Videos Slider
     ----------------------------*/
@@ -70,7 +107,7 @@
         items: 4,
         dots: false,
         nav: true,
-        navText: ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+        navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
         smartSpeed: 1200,
         autoHeight: false,
         autoplay: true,
@@ -91,8 +128,8 @@
     });
 
     /*------------------
-		Magnific
-	--------------------*/
+        Magnific
+    --------------------*/
     $('.video-popup').magnificPopup({
         type: 'iframe'
     });
@@ -106,7 +143,7 @@
     var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
     var yyyy = today.getFullYear();
 
-    if(mm == 12) {
+    if (mm == 12) {
         mm = '01';
         yyyy = yyyy + 1;
     } else {
@@ -115,18 +152,18 @@
     }
     var timerdate = mm + '/' + dd + '/' + yyyy;
     // For demo preview end
-    
+
 
     // Use this for real timer date
     /* var timerdate = "2020/01/01"; */
 
-	$("#countdown-time").countdown(timerdate, function(event) {
+    $("#countdown-time").countdown(timerdate, function (event) {
         $(this).html(event.strftime("<div class='countdown__item'><span>%D</span> <p>Days</p> </div>" + "<div class='countdown__item'><span>%H</span> <p>Hours</p> </div>" + "<div class='countdown__item'><span>%M</span> <p>Minutes</p> </div>" + "<div class='countdown__item'><span>%S</span> <p>Seconds</p> </div>"));
     });
 
     /*------------------
-		Barfiller
-	--------------------*/
+        Barfiller
+    --------------------*/
     $('#bar1').barfiller({
         barColor: "#ffffff",
     });
@@ -140,8 +177,8 @@
     });
 
     /*-------------------
-		Nice Scroll
-	--------------------- */
+        Nice Scroll
+    --------------------- */
     $(".nice-scroll").niceScroll({
         cursorcolor: "#111111",
         cursorwidth: "5px",
@@ -152,3 +189,46 @@
     });
 
 })(jQuery);
+
+
+/* PaoloRos */
+
+/*
+* Javascript to show and hide cookie banner using localstorage
+*/
+
+/**
+ * @description Shows the cookie banner
+ */
+function showCookieBanner() {
+    let cookieBanner = document.getElementById("cookie-banner");
+    cookieBanner.style.display = "block";
+}
+
+/**
+ * @description Hides the Cookie banner and saves the value to localstorage
+ */
+function hideCookieBanner() {
+    localStorage.setItem("xgiove_cb_isCookieAccepted", "yes");
+
+    let cookieBanner = document.getElementById("cookie-banner");
+    cookieBanner.style.display = "none";
+}
+
+/**
+ * @description Checks the localstorage and shows Cookie banner based on it.
+ */
+function initializeCookieBanner() {
+    let isCookieAccepted = localStorage.getItem("xgiove_cb_isCookieAccepted");
+    if (isCookieAccepted === null) {
+        localStorage.setItem("xgiove_cb_isCookieAccepted", "no");
+        showCookieBanner();
+    }
+    if (isCookieAccepted === "no") {
+        showCookieBanner();
+    }
+}
+
+// Assigning values to window object
+window.onload = initializeCookieBanner();
+window.cb_hideCookieBanner = hideCookieBanner;
